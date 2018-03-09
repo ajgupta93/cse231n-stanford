ff4ca7f5ca3802f2c45c87f3a5bdd301b717c9bc
Ajitesh Gupta <ajgupta93@gmail.com>
2018-03-08 16:55:33 -0800

    softmax

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -22,14 +22,34 @@ def softmax_loss_naive(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  delta = 0.0000000001
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  for i in range(X.shape[0]):
+    x = X[i,:].reshape(-1,1)
+    prods = np.zeros((W.shape[1]))
+    max_prod = -np.inf
+    for c in range(W.shape[1]):
+        w_class = W[:,c].reshape(-1,1)
+        prods[c] = w_class.T.dot(x)
+        if prods[c]>max_prod:
+            max_prod = prods[c]
+    prods-=max_prod
+    num = np.power(np.e,prods[y[i]])
+    denom = np.sum(np.power(np.e,prods))
+    loss+=-np.log(num/denom)
+    p = np.power(np.e,prods)/denom
+    p[y[i]]-=1
+    for c in range(W.shape[1]):
+        dW[:,c] += p[c]*x.ravel()
+  loss += 0.5*reg*np.sum(W**2)
+  loss/=X.shape[0]
+  dW/=X.shape[0]
+  dW+=reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,7 +73,16 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  wx = W.T.dot(X.T)
+  wxmax = wx.max(0).reshape(1,-1)
+  wx -= wxmax
+  nx = np.power(np.e,wx)
+  dx = nx.sum(0).reshape(1,-1)
+  px = np.divide(nx,dx)
+  ym = np.zeros((W.shape[1],X.shape[0]))
+  ym[y,range(X.shape[0])] = 1
+  loss = -np.sum(np.log(px[y,range(X.shape[0])]))/X.shape[0]+0.5*reg*np.sum(W**2)
+  dW = ((px-ym).dot(X)).T/X.shape[0]+reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
