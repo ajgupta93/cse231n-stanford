fbc0e480fb7e098a40b1e1c2ee6743468ffe19c7
Ajitesh Gupta <ajgupta93@gmail.com>
2018-03-08 18:47:21 -0800

    svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,13 +34,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,j]+=X[i,:]
+        dW[:,y[i]]+=-X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += 2*reg*W
 
   #############################################################################
   # TODO:                                                                     #
@@ -69,7 +73,12 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  wx = W.T.dot(X.T)
+  wx_class = wx[y,range(X.shape[0])].reshape(1,-1)
+  zero_m = np.zeros_like(wx_class)
+  margin = np.maximum(zero_m,np.subtract(wx,wx_class)+1)
+  margin[y,range(X.shape[0])] = 0
+  loss = margin.sum()/X.shape[0]+reg*np.sum(W**2)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +93,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+  mask = (margin>0).astype(np.int32)
+  mask[y,range(mask.shape[1])] = -mask.sum(axis=0)
+  dW = (mask.dot(X)).T
+  dW /= X.shape[0]
+  dW += 2*reg*W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
